c4828b7deebeb0aeae687d63d8dbed743ce0ffbf
Дмитрий <dimonchik_dyatchin6464@mail.ru>
2019-02-28 22:06:22 +0300

    count slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-$('.slider').slick();
+$('.slider').slick({
+    prevArrow: $('.bottom__box-arrow-prev'),
+    nextArrow: $('.bottom__box-arrow-next'),
+});
+
+
+$(".slider").on('afterChange', function (event, slick, currentSlide) {
+    $("#number").text(currentSlide + 1);
+});
+
+
