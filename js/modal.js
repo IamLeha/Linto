e1730eaa0f1cb96fdc593d9c7a05ca8c5d848338
Дмитрий <dimonchik_dyatchin6464@mail.ru>
2019-03-07 12:56:46 +0300

    store

--- js/modal.js
+++ js/modal.js
@@ -28,3 +28,19 @@ $(document).ready(function () {
     });
 
 });
+
+
+
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+});
