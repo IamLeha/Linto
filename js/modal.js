648edcfdf97d4601cc2ebd818ae127ff350194f6
Дмитрий <dimonchik_dyatchin6464@mail.ru>
2019-03-07 11:00:50 +0300

    видео

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,30 @@
 $(".gallery").fancybox({
-    openEffect	: 'elastic'
+    openEffect: 'elastic'
+});
+
+$(document).ready(function () {
+
+
+    $(".fancybox-gallery").fancybox({
+
+        afterShow: function () {
+            this.content.find('video').trigger('play')
+            this.content.find('video').on('ended', function () {
+                $.fancybox.next();
+            });
+        }
+
+    });
+
+    $(".fancybox-single").fancybox({
+
+        afterShow: function () {
+            this.content.find('video').trigger('play')
+            this.content.find('video').on('ended', function () {
+                $.fancybox.close();
+            });
+        }
+
+    });
+
 });
